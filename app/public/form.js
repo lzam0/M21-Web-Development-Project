ccb499ac32390d70ece31f8a50347bdb36699e79
LZ <itsleihl@gmail.com>
2023-12-11 08:52:57 +0000

    fixed silly errors

--- app/public/form.js
+++ app/public/form.js
@@ -1,52 +1,3 @@
-function processSubmit(e){
-    e.preventDefault();
-
-    const firstName = document.querySelector("#first").value;
-
-    const lastName = document.querySelector("#last").value;
-
-    const email = document.querySelector("#email").value;
-;
-    const message = document.querySelector("#message").value;
-
-    const formData = {
-        first: firstName,
-        last: lastName,
-        email: email,
-        message: message
-    };
-
-    const serializedMessage = JSON.stringify(formData);
-
-    const fetchOptions ={
-        method: "POST",
-        headers: {
-            "Accept": "Application/json",
-            "Content-Type": "application/json"
-        },
-        body: serializedMessage
-    }
-
-    
-    fetch("http://localhost:3000/form", fetchOptions)
-    .then(onResponse)
-    .then(onTextReady);
-}
-
-function onTextReady(text){
-    alert("Form Submission Successful")
-}
-
-function onResponse(response){
-    return response.text();
-}
-
-/* Grab form submit button */
-const form = document.querySelector("#form");
-
-addEventListener('submit', processSubmit);
-
-
 var formpagedata;
 
 function initload()
@@ -55,6 +6,7 @@ function initload()
 fetch("http://localhost:3000/language.json")
 .then((res) => {
 return res.json();
+
 })
 .then(data => {
 formpagedata1 = data;
@@ -110,3 +62,51 @@ function onClickLang(UserOptions) {
   })}
 
 }
+
+function processSubmit(e){
+  e.preventDefault();
+
+  const firstName = document.querySelector("#first").value;
+
+  const lastName = document.querySelector("#last").value;
+
+  const email = document.querySelector("#email").value;
+;
+  const message = document.querySelector("#message").value;
+
+  const formData = {
+      first: firstName,
+      last: lastName,
+      email: email,
+      message: message
+  };
+
+  const serializedMessage = JSON.stringify(formData);
+
+  const fetchOptions ={
+      method: "POST",
+      headers: {
+          "Accept": "Application/json",
+          "Content-Type": "application/json"
+      },
+      body: serializedMessage
+  }
+
+  
+  fetch("http://localhost:3000/form", fetchOptions)
+  .then(onResponse)
+  .then(onTextReady);
+}
+
+function onTextReady(text){
+  alert("Form Submission Successful")
+}
+
+function onResponse(response){
+  return response.text();
+}
+
+/* Grab form submit button */
+const form = document.querySelector("#form");
+
+addEventListener('submit', processSubmit);
